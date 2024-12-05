0b2c9cab8794f20cc4cc6ace8b797fe4fe0a0973
paul <paulmichaelblackmore@gmail.com>
2024-12-05 12:46:28 +0000

    feat: steup file based routing for properties

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center text-4xl h-screen">
-      Hello World!
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-4xl">Hello World!</h1>
+        <Link href="/properties">Properties</Link>
+      </div>
     </div>
   )
 }
