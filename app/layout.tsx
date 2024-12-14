423c21544e2fe675a76fcce5c236f36b2b1e3283
paul <paulmichaelblackmore@gmail.com>
2024-12-14 20:13:15 +0000

    feat: add styled footer to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import { Navbar } from '@/components/Navbar';
+import { Footer } from '@/components/Footer';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+
+export const Footer = () => (
+  <footer className='bg-slate-800'>
+    <div className='mx-auto w-full max-w-screen-xl p-4 md:flex md:items-center md:justify-between'>
+      <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+        © 2025
+        <Link href='https://flowbite.com/' className='hover:underline'>
+          Property App
+        </Link>
+        . All Rights Reserved.
+      </span>
+      <ul className='mt-3 flex flex-wrap items-center text-sm font-medium text-gray-500 sm:mt-0 dark:text-gray-400'>
+        <li>
+          <Link href='/' className='me-4 hover:underline md:me-6'>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href='/properties' className='me-4 hover:underline md:me-6'>
+            Properties
+          </Link>
+        </li>
+        <li>
+          <Link href='#' className='me-4 hover:underline md:me-6'>
+            Privacy Policy
+          </Link>
+        </li>
+        <li>
+          <Link href='#' className='me-4 hover:underline md:me-6'>
+            Licensing
+          </Link>
+        </li>
+        <li>
+          <Link href='#' className='hover:underline'>
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </footer>
+);
