3b1793f0744193ecef9de36904e2a15296fb6a89
paul <paulmichaelblackmore@gmail.com>
2024-12-14 13:56:10 +0000

    feat: update nav component with conditional rendering

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className='flex h-screen items-center justify-center'>
+    <div className='flex h-[calc(100vh-4rem)] items-center justify-center'>
       <div className='flex flex-col items-center justify-center'>
         <h1 className='text-4xl'>Hello World!</h1>
         <Link href='/properties'>Properties</Link>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,7 @@
-import { joinClassNames } from '@/helpers';
+'use client';
+import { PropsWithChildren, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 import {
   Disclosure,
   DisclosureButton,
@@ -9,23 +12,22 @@ import {
   MenuItems,
 } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
-import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import { joinClassNames } from '@/helpers';
 
-const navigation = [
-  { name: 'Dashboard', href: '#', current: false },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-];
+type LinkProps = { href: string };
+type NavLinkProps = { isLoggedIn: boolean };
 
 const Navbar = () => {
+  const [isLoggedIn] = useState(false);
   return (
     <Disclosure as='nav' className='bg-slate-800'>
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
         <div className='relative flex h-16 items-center justify-between'>
           <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
-            <DisclosureButton className='group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
-              <span className='absolute -inset-0.5' />
+            {/* Burger menu controls */}
+            <DisclosureButton className='group relative items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+              <span className='absolute' />
               <Bars3Icon
                 aria-hidden='true'
                 className='block size-6 group-data-[open]:hidden'
@@ -36,103 +38,107 @@ const Navbar = () => {
               />
             </DisclosureButton>
           </div>
-
+          {/* Desktop links */}
           <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
             <div className='hidden sm:ml-6 sm:block'>
-              <div className='flex space-x-4'>
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    aria-current={item.current ? 'page' : undefined}
-                    className={joinClassNames(
-                      item.current
-                        ? 'bg-gray-900 text-white'
-                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'rounded-md px-3 py-2 text-sm font-medium'
-                    )}
-                  >
-                    {item.name}
-                  </a>
-                ))}
+              <div className='flex space-x-4 text-white'>
+                <NavLinks isLoggedIn={isLoggedIn} />
               </div>
             </div>
           </div>
-          <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-            <button
-              type='button'
-              className='relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
-            >
-              <BellIcon aria-hidden='true' className='size-6' />
-            </button>
-            <Menu as='div' className='relative ml-3'>
-              <div>
-                <MenuButton className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
-                  <Image
-                    alt='profile-image'
-                    width={0}
-                    height={0}
-                    src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                    className='size-8 rounded-full'
-                  />
-                </MenuButton>
-              </div>
-              <MenuItems
-                transition
-                className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in'
+          <div className='tems-center flex justify-end'>
+            {!isLoggedIn ? (
+              <button
+                type='button'
+                className='rounded border border-slate-200 bg-transparent px-4 py-2 text-xs font-semibold text-slate-200 hover:border-slate-300 hover:text-slate-300'
               >
-                <MenuItem>
-                  <a
-                    href='#'
-                    className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none'
+                Login
+              </button>
+            ) : (
+              <>
+                <button
+                  type='button'
+                  className='relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
+                >
+                  <BellIcon aria-hidden='true' className='size-6' />
+                </button>
+                {/* Dropdown menu */}
+                <Menu as='div' className='relative ml-3'>
+                  <div>
+                    <MenuButton className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
+                      <Image
+                        alt='profile-image'
+                        width={0}
+                        height={0}
+                        src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                        className='size-8 rounded-full'
+                      />
+                    </MenuButton>
+                  </div>
+                  <MenuItems
+                    transition
+                    className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in'
                   >
-                    Your Profile
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href='#'
-                    className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none'
-                  >
-                    Settings
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href='#'
-                    className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none'
-                  >
-                    Sign out
-                  </a>
-                </MenuItem>
-              </MenuItems>
-            </Menu>
+                    <MenuItem>
+                      <MenuLink href='#'>Your Profile</MenuLink>
+                    </MenuItem>
+                    <MenuItem>
+                      <MenuLink href='#'>Settings</MenuLink>
+                    </MenuItem>
+                    <MenuItem>
+                      <MenuLink href='#'>Sign out</MenuLink>
+                    </MenuItem>
+                  </MenuItems>
+                </Menu>
+              </>
+            )}
           </div>
         </div>
       </div>
-
+      {/* Mobile links */}
       <DisclosurePanel className='sm:hidden'>
-        <div className='absolute w-full space-y-1 bg-slate-800 px-2 pb-3 pt-2'>
-          {navigation.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as='a'
-              href={item.href}
-              aria-current={item.current ? 'page' : undefined}
-              className={joinClassNames(
-                item.current
-                  ? 'bg-gray-900 text-white'
-                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                'block rounded-md px-3 py-2 text-base font-medium'
-              )}
-            >
-              {item.name}
-            </DisclosureButton>
-          ))}
+        <div className='text-x absolute flex h-[calc(100vh-4rem)] w-full flex-col items-center gap-10 bg-slate-800 px-2 pt-10 text-white'>
+          <NavLinks isLoggedIn={isLoggedIn} />
         </div>
       </DisclosurePanel>
     </Disclosure>
   );
 };
+
+const NavLinks = ({ isLoggedIn }: NavLinkProps) => {
+  const pathname = usePathname();
+  const isLinkActive = (path: string) =>
+    joinClassNames(
+      pathname === path
+        ? 'bg-orange-400 text-white'
+        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+      'rounded-md px-3 py-2 text-sm font-medium'
+    );
+
+  return (
+    <>
+      <Link href='/' className={isLinkActive('/')}>
+        Home
+      </Link>
+      <Link href='/properties' className={isLinkActive('/properties')}>
+        Properties
+      </Link>
+      {isLoggedIn && (
+        <Link href='/add-property' className={isLinkActive('/add-property')}>
+          Add Property
+        </Link>
+      )}
+    </>
+  );
+};
+
+const MenuLink = ({ children, href }: PropsWithChildren<LinkProps>) => (
+  <Link
+    href={href}
+    className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none'
+  >
+    {children}
+  </Link>
+);
 
 export default Navbar;
