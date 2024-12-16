578cf8dac4b3d868b426b9c10abdd9a52bc1ba76
paul <paulmichaelblackmore@gmail.com>
2024-12-16 16:47:10 +0000

    fix: footer spacing and remove list

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,39 +4,25 @@ export const Footer = () => (
   <footer className='hidden bg-slate-800 md:block'>
     <div className='mx-auto w-full max-w-screen-xl p-4 md:flex md:items-center md:justify-between'>
       <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
-        © 2025
-        <Link href='https://flowbite.com/' className='hover:underline'>
-          Property App
-        </Link>
-        . All Rights Reserved.
+        © 2025 Property App. All Rights Reserved.
       </span>
-      <ul className='mt-3 flex flex-wrap items-center text-sm font-medium text-gray-500 sm:mt-0 dark:text-gray-400'>
-        <li>
-          <Link href='/' className='me-4 hover:underline md:me-6'>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href='/properties' className='me-4 hover:underline md:me-6'>
-            Properties
-          </Link>
-        </li>
-        <li>
-          <Link href='#' className='me-4 hover:underline md:me-6'>
-            Privacy Policy
-          </Link>
-        </li>
-        <li>
-          <Link href='#' className='me-4 hover:underline md:me-6'>
-            Licensing
-          </Link>
-        </li>
-        <li>
-          <Link href='#' className='hover:underline'>
-            Contact
-          </Link>
-        </li>
-      </ul>
+      <div className='mt-3 flex flex-wrap items-center text-sm font-medium text-gray-500 sm:mt-0 dark:text-gray-400'>
+        <Link href='/' className='me-4 hover:underline md:me-6'>
+          Home
+        </Link>
+        <Link href='/properties' className='me-4 hover:underline md:me-6'>
+          Properties
+        </Link>
+        <Link href='#' className='me-4 hover:underline md:me-6'>
+          Privacy Policy
+        </Link>
+        <Link href='#' className='me-4 hover:underline md:me-6'>
+          Licensing
+        </Link>
+        <Link href='#' className='hover:underline'>
+          Contact
+        </Link>
+      </div>
     </div>
   </footer>
 );
