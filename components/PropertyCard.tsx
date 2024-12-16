416244b44f493f2443d53ff356c8badc21ecf52e
paul <paulmichaelblackmore@gmail.com>
2024-12-16 15:31:13 +0000

    chore: remove unused css

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -14,7 +14,7 @@ export const PropertyCard = ({ property }: Props) => {
       <div className='relative h-72'>
         <Image
           fill
-          className='relative h-72 object-cover'
+          className='relative h-60 object-cover'
           src={`/properties/${property.images[0]}`}
           alt={property.name}
         />
@@ -26,26 +26,26 @@ export const PropertyCard = ({ property }: Props) => {
           </div>
         )}
       </div>
-      <div className='relative flex flex-col justify-center gap-2 p-4'>
+      <div className='relative flex h-36 flex-col justify-center gap-1 p-4'>
         <span>{property.type}</span>
         <h2 className='text-xl font-bold'>{property.name}</h2>
         <p>{property.description}</p>
       </div>
-      <div className='h-40 p-4'>
+      <div className='h-30 p-4'>
         <span>Ammedities</span>
-        <div className='mb-4 mt-4 flex items-center justify-between gap-2 font-semibold'>
-          <div className='flex items-center gap-1'>
+        <div className='mb-4 mt-4 flex items-center justify-between'>
+          <div className='flex gap-1'>
             <FaBed size={25} />
             <span>{`${property.beds} bed(s)`}</span>
           </div>
-          <div className='flex items-center gap-1'>
+          <div className='flex gap-1'>
             <FaBath size={20} />
             <span>{`${property.baths} bath(s)`}</span>
           </div>
-          <div>{`${numberWithCommas(property.square_feet)} sqft`}</div>
+          <span>{`${numberWithCommas(property.square_feet)} sqft`}</span>
         </div>
       </div>
-      <div className='absolute bottom-3 left-3 flex items-center justify-start gap-1'>
+      <div className='mb-3 ml-3 flex items-center justify-start gap-1'>
         <FaLocationDot size={20} />
         <span className='font-bold'>{`${property.location.city}, ${property.location.state}`}</span>
       </div>
