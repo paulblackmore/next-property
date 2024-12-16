321d9fc317d302b8ee455f38cf91b58bb647d01c
paul <paulmichaelblackmore@gmail.com>
2024-12-16 17:42:42 +0000

    feat: add custom css for loading spinner anaimation

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loading = () => (
+  <div className='flex h-[calc(100vh-8rem)] flex-col items-center justify-center'>
+    <span className='loader' />
+  </div>
+);
+
+export default Loading;
